4872294022177f3613957e9b81ac4f08031d2c08
Cristo <Cristobal343@outlook.com>
2024-05-09 12:10:38 -0700

    my third commit by cristo

--- src/main/java/edu/upVictoria/POO/Services/Database.java
+++ src/main/java/edu/upVictoria/POO/Services/Database.java
@@ -3,6 +3,9 @@ package edu.upVictoria.POO.Services;
 import edu.upVictoria.POO.Services.Interfaces.ISQLcreate_drop;
 import edu.upVictoria.POO.Services.Interfaces.SQLconsult;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,9 +25,12 @@ public class Database extends SQLMannager implements ISQLcreate_drop {
     }
 
     public void CREATE_TABLE(){
-
-
-
+        try {
+            File file = new File(String.format("%s%s"));
+            System.out.println(String.format("Table %s create!"));
+        }catch (RuntimeException e){
+            throw new RuntimeException("Something wrong happend");
+        }
     }
 
     public boolean DROP_TABLE(String name) {
--- src/main/java/edu/upVictoria/POO/Services/Table.java
+++ src/main/java/edu/upVictoria/POO/Services/Table.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class Table extends SQLMannager implements ISQLcreate_drop, SQLconsult {
+public class Table extends SQLMannager implements ISQLcreate_drop {
 
     public String name;
 
